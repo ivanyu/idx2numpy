b53a666eb55cd60752e1da98588a8d8a8bddc65c
ivanyu <ivanhoe12@gmail.com>
2013-02-04 00:31:23 +0400

    Tests added.

--- idx2numpy/converters.py
+++ idx2numpy/converters.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import
 from __future__ import with_statement
 
+import struct
+import numpy
+import operator
+import contextlib
+from StringIO import StringIO
+
 from .FormatError import FormatError
 
 
@@ -10,7 +16,11 @@ def convert_from_file(file):
     returns it.
     file is a file-like object (with read() method) or a file name.
     '''
-    return None
+    if isinstance(file, basestring):
+        with open(file, 'rb') as f:
+            return _internal_convert(f)
+    else:
+        return _internal_convert(file)
 
 
 def convert_from_string(idx_string):
@@ -18,7 +28,8 @@ def convert_from_string(idx_string):
     Converts string which presents file in IDX format into numpy.ndarray and
     returns it.
     '''
-    return None
+    with contextlib.closing(StringIO(idx_string)) as sio:
+        return _internal_convert(sio)
 
 
 def _internal_convert(input):
@@ -26,4 +37,70 @@ def _internal_convert(input):
     Converts file in IDX format provided by file-like input into numpy.ndarray
     and returns it.
     '''
-    return None
+    '''
+    Converts file in IDX format provided by file-like input into numpy.ndarray
+    and returns it.
+    '''
+
+    # Possible data types.
+    # Keys are IDX data type codes.
+    # Values: (ndarray data type name, name for struct.unpack, size in bytes).
+    DATA_TYPES = {
+        0x08: ('ubyte', 'B', 1),
+        0x09: ('byte', 'b', 1),
+        0x0B: ('int16', 'h', 2),
+        0x0C: ('int32', 'i', 4),
+        0x0D: ('float', 'f', 4),
+        0x0E: ('double', 'd', 8)
+    }
+
+    # Read the "magic number" - 4 bytes.
+    try:
+        mn = struct.unpack('>BBBB', input.read(4))
+    except struct.error:
+        raise FormatError(struct.error)
+
+    # First two bytes are always zero, check it.
+    if mn[0] != 0 or mn[1] != 0:
+        msg = ("Incorrect first two bytes of the magic number: " +
+               "0x{0:02X} 0x{1:02X}".format(mn[0], mn[1]))
+        raise FormatError(msg)
+
+    # 3rd byte is the data type code.
+    dtype_code = mn[2]
+    if dtype_code not in DATA_TYPES:
+        msg = "Incorrect data type code: 0x{0:02X}".format(dtype_code)
+        raise FormatError(msg)
+
+    # 4th byte is the number of dimensions.
+    dims = int(mn[3])
+
+    # See possible data types description.
+    dtype, dtype_s, el_size = DATA_TYPES[dtype_code]
+
+    # 4-byte integer for length of each dimension.
+    try:
+        dims_sizes = struct.unpack('>'+'I'*dims, input.read(4*dims))
+    except struct.error as e:
+        raise FormatError('Dims sizes: {}'.format(e))
+
+    # Full length of data.
+    full_length = reduce(operator.mul, dims_sizes, 1)
+
+    result_array = numpy.ndarray(shape=[full_length], dtype=dtype)
+
+    # Read data "in the line".
+    unpack_str = '>' + dtype_s*full_length
+    try:
+        result_array[0:full_length] = struct.unpack(
+            unpack_str, input.read(full_length * el_size))
+    except struct.error as e:
+        raise FormatError('Data: {}'.format(e))
+
+    # Check for superfluous data.
+    if len(input.read(1)) > 0:
+        raise FormatError('Superfluous data detected.')
+
+    # Reshape data according to dimensions sizes.
+    result = numpy.reshape(result_array, dims_sizes)
+    return result
---
+++ tests/converters_test.py
@@ -0,0 +1,125 @@
+import unittest
+import idx2numpy
+import numpy as np
+import os
+import struct
+
+
+def _to_list(nd):
+    return [ x for x in nd ]
+
+class TestConvertFromFile(unittest.TestCase):
+
+    def setUp(self):
+        self.files_dir = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'files')
+
+    def test_empty_file_on_disk(self):
+        file = os.path.join(self.files_dir, 'empty.idx')
+        self.assertRaises(idx2numpy.FormatError,
+            idx2numpy.convert_from_file, file)
+
+    def test_correct_file_on_disk(self):
+        file = os.path.join(self.files_dir, 'correct.idx')
+        self.assertSequenceEqual(
+            [0x0A, 0x0B, 0x0C], 
+            _to_list(idx2numpy.convert_from_file(file)))
+
+
+class TestConvertFromString(unittest.TestCase):
+
+    def test_empty_string(self):
+        self.assertRaises(idx2numpy.FormatError,
+            idx2numpy.convert_from_string, b'')
+
+    def test_incorrect_magic_number(self):
+        self.assertRaises(idx2numpy.FormatError,
+            idx2numpy.convert_from_string, b'\x00\x00')
+        self.assertRaises(idx2numpy.FormatError,
+            idx2numpy.convert_from_string, b'\x01\x00\x08\x00')
+        self.assertRaises(idx2numpy.FormatError,
+            idx2numpy.convert_from_string, b'\x00\x01\x08\x00')
+        # Incorrect type code.
+        self.assertRaises(idx2numpy.FormatError,
+            idx2numpy.convert_from_string, b'\x00\x00\x01\x00')
+        # Incorrect dimension size.
+        self.assertRaises(idx2numpy.FormatError,
+            idx2numpy.convert_from_string, b'\x00\x00\x08\x01\x00\x00\x00')
+        # Incorrect data length.
+        self.assertRaises(idx2numpy.FormatError,
+            idx2numpy.convert_from_string,
+            b'\x00\x00\x08\x01\x00\x00\x00\x02\x01')
+        # Superfluous data
+        self.assertRaises(idx2numpy.FormatError,
+            idx2numpy.convert_from_string,
+            b'\x00\x00\x08\x01\x00\x00\x00\x02\x01\x02\x03\x04')
+
+    def test_correct(self):
+        # Unsigned byte.
+        result = idx2numpy.convert_from_string(
+            b'\x00\x00\x08\x01\x00\x00\x00\x03' +
+            b'\x0A' +
+            b'\x0B' +
+            b'\xFF')
+        self.assertEquals(np.ndim(result), 1)
+        self.assertEquals(np.shape(result), (3,))
+        self.assertSequenceEqual(
+            _to_list(result),
+            [0x0A, 0x0B, 0xFF])
+        
+        # Signed byte.
+        result = idx2numpy.convert_from_string(
+            b'\x00\x00\x09\x01\x00\x00\x00\x04' +
+            b'\xFE' +
+            b'\xFF' +
+            b'\x00' +
+            b'\xAA')
+        self.assertEquals(np.ndim(result), 1)
+        self.assertEquals(np.shape(result), (4,))
+        self.assertSequenceEqual(
+            _to_list(result),
+            [-2, -1, 0x00, -86])
+        
+        # Short.
+        result = idx2numpy.convert_from_string(
+            b'\x00\x00\x0B\x01\x00\x00\x00\x02' +
+            b'\xF0\x05' +
+            b'\x00\xFF')
+        self.assertEquals(np.ndim(result), 1)
+        self.assertEquals(np.shape(result), (2,))
+        self.assertSequenceEqual(
+            _to_list(result),
+            [-4091, 255])
+
+        # Integer.
+        result = idx2numpy.convert_from_string(
+            b'\x00\x00\x0C\x01\x00\x00\x00\x03' +
+            b'\x00\xFF\x00\xFF' +
+            b'\x80\x00\x00\x00' +
+            b'\x00\x00\x00\x00')
+        self.assertEquals(np.ndim(result), 1)
+        self.assertEquals(np.shape(result), (3,))
+        self.assertSequenceEqual(
+            _to_list(result),
+            [0x00FF00FF, -0x80000000, 0x00])
+
+        # Float.
+        # So fat, no tests.
+
+        # Double.
+        result = idx2numpy.convert_from_string(
+            b'\x00\x00\x0E\x01\x00\x00\x00\x05' +
+            b'\x3F\xF0\x00\x00\x00\x00\x00\x00' +
+            b'\x40\x00\x00\x00\x00\x00\x00\x00' +
+            b'\xC0\x00\x00\x00\x00\x00\x00\x00' +
+            b'\x00\x00\x00\x00\x00\x00\x00\x00' +
+            b'\x80\x00\x00\x00\x00\x00\x00\x00')
+        self.assertEquals(np.ndim(result), 1)
+        self.assertEquals(np.shape(result), (5,))
+        self.assertSequenceEqual(
+            _to_list(result),
+            [1.0, 2.0, -2.0, 0.0, -0.0])
+
+
+if __name__ == '__main__':
+    unittest.main()
